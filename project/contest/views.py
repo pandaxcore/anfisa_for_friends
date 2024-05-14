61dbf4488ca85d64a783a28e64703ada8e8b4a69
pandaxcore <pandadesign7@gmail.com>
2024-05-14 18:40:28 +0600

    csrf-token middleware add

--- project/contest/views.py
+++ project/contest/views.py
@@ -1,9 +1,11 @@
+"""Docstring."""
 from django.shortcuts import render
 
 from .forms import ContestForm
 
 
 def proposal(request):
-    form = ContestForm(request.GET or None)
+    """Docstring."""
+    form = ContestForm(request.POST or None)
     context = {'form': form}
     return render(request, 'contest/form.html', context)
